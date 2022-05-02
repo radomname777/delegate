a7fa9466b5fa4d52fef17faca6778e100fbc1a21
NIHAd AXUNDZADE <nihadaxundzade06@outlook.com>
2022-05-03 01:57:24 +0400

    Add project files.

---
+++ delegate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyApp 
+{
+
+    delegate void Func(string str);
+    class MyClass
+    {
+        public void Reverse(string code)
+        {
+            char[] code2 = code.Reverse().ToArray();
+            Console.WriteLine(code2);
+        }
+        public void Space(string code)
+        {
+            char[] s1 = code.ToCharArray();
+            Console.WriteLine(string.Join("_", s1));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter string: ");
+            var str = Console.ReadLine();
+            MyClass cls = new MyClass();
+            Func funcDell = cls.Reverse;
+            funcDell += cls.Space;
+            Run run = new Run();
+            run.runFunc(funcDell, str);
+
+        }
+    }
+}
---
+++ delegate/Run.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp
+{
+    class Run
+    {
+        public void runFunc(Func funcDell, string str)
+        {
+            funcDell(str);
+        }
+    }
+}
